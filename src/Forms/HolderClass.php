0b95fd343093400c193e5e77e4a054099f33b361
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2022-05-06 14:52:58 +0200

    add: form holder trait

---
+++ src/Forms/HolderClass.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Syntro\SilverstripeFrontendForms\Forms;
+
+/**
+ * Adds the capability to modify a class on the holder
+ * @author Matthias Leutenegger
+ */
+trait HolderClass
+{
+
+    /**
+     * Stores the css-classes for the field holder
+     * @var array
+     */
+    protected $holderClasses;
+
+    /**
+     * @config
+     * @var array $default_holder_classes The default classes to apply to the FormField
+     */
+    private static $default_holder_classes = [];
+
+    /**
+     * Set up the default classes for the form. This is done on construct so that the default classes can be removed
+     * after instantiation
+     * @return void
+     */
+    protected function setupDefaultHolderClasses()
+    {
+        $defaultClasses = $this->config()->get('default_holder_classes');
+        if ($defaultClasses) {
+            foreach ($defaultClasses as $class) {
+                $this->addHolderClass($class);
+            }
+        }
+    }
+
+    /**
+     * Check if a CSS-class has been added to the field holder.
+     *
+     * @param string $class A string containing a classname or several class
+     * names delimited by a single space.
+     * @return boolean True if all of the classnames passed in have been added.
+     */
+    public function hasHolderClass($class)
+    {
+        //split at white space
+        $classes = preg_split('/\s+/', $class ?? '');
+        foreach ($classes as $class) {
+            if (!isset($this->holderClasses[$class])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Add one or more CSS-classes to the FormField holder.
+     *
+     * Multiple class names should be space delimited.
+     *
+     * @param string $class the class or classes to add
+     *
+     * @return $this
+     */
+    public function addHolderClass($class)
+    {
+        $classes = preg_split('/\s+/', $class ?? '');
+
+        foreach ($classes as $class) {
+            $this->holderClasses[$class] = $class;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove one or more CSS-classes from the FormField holder.
+     *
+     * @param string $class the class or classes to remove
+     *
+     * @return $this
+     */
+    public function removeHolderClass($class)
+    {
+        $classes = preg_split('/\s+/', $class ?? '');
+
+        foreach ($classes as $class) {
+            unset($this->holderClasses[$class]);
+        }
+
+        return $this;
+    }
+}
