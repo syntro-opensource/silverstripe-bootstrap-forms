4320279e1e91f69cd7e47b13ffc7db2f57127eb8
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2022-05-06 18:49:10 +0200

    cs

--- src/Forms/OptionsetField.php
+++ src/Forms/OptionsetField.php
@@ -17,10 +17,10 @@ class OptionsetField extends BackendOptionsetField
     /**
      * Returns an input field.
      *
-     * @param string            $name   The field name
-     * @param string            $title  The field title
+     * @param string             $name   The field name
+     * @param string             $title  The field title
      * @param array|\ArrayAccess $source A map of the dropdown items
-     * @param mixed             $value  The current value
+     * @param mixed              $value  The current value
      */
     function __construct($name, $title = null, $source = [], $value = null)
     {
