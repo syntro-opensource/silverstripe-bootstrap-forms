26abb5bd686d8cf60ceb07540a513856b5512cba
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2022-05-06 15:52:26 +0200

    add: frontend EmailField

---
+++ src/Forms/EmailField.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Syntro\SilverstripeFrontendForms\Forms;
+
+use SilverStripe\Forms\Form;
+use SilverStripe\Forms\EmailField as BackendEmailField;
+
+/**
+ *
+ */
+class EmailField extends BackendEmailField
+{
+
+    use HolderClass;
+
+    /**
+     * Returns an input field.
+     *
+     * @param string $name name of the field
+     * @param null|string $title title of the field
+     * @param string $value value of the field
+     * @param null|int $maxLength Max characters to allow for this field. If this value is stored
+     * against a DB field with a fixed size it's recommended to set an appropriate max length
+     * matching this size.
+     * @param null|Form $form the form
+     */
+    function __construct($name, $title = null, $value = '', $maxLength = null, $form = null)
+    {
+        $this->setTemplate('Syntro\\SilverstripeFrontendForms\\Forms\\TextField');
+        $this->setFieldHolderTemplate('Syntro\\SilverstripeFrontendForms\\Forms\\FormField_holder');
+        $this->setSmallFieldHolderTemplate('Syntro\\SilverstripeFrontendForms\\Forms\\FormField_holder_small');
+        $this->setupDefaultHolderClasses();
+        parent::__construct($name, $title, $value, $maxLength, $form);
+    }
+}
