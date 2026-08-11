1ceb2ff2818dbf033505e32882544dd749c7b089
Patrick Côté <git@ptct.ch>
2026-08-11 17:45:07 +0200

    fix: class name

--- src/Dev/FormPageController.php
+++ src/Dev/FormPageController.php
@@ -64,7 +64,7 @@ class FormPageController extends ContentController implements TestOnly
         $readonlyfield->addHolderClass('readonlyfieldholderclass')->addExtraClass('readonlyfieldextraclass');
         $textareafield->addHolderClass('textareafieldholderclass')->addExtraClass('textareafieldextraclass');
         $textfield->addHolderClass('textfieldholderclass')->addExtraClass('textfieldextraclass');
-        $numericfield->addHolderClass('numericfieldholderclass')->addExtraClass('numericfieldholderclass');
+        $numericfield->addHolderClass('numericfieldholderclass')->addExtraClass('numericfieldextraclass');
 
         $actions = FieldList::create(FormAction::create('submit', 'Submit'));
         $required = RequiredFieldsValidator::create(['required']);
